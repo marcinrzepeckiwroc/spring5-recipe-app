e13dac1b3fbe3242249526323f26bedddb033657
marcinrzepeckiwroc <marcin_rzepecki@interia.pl>
2020-01-07 13:02:55 +0100

    relation OnetoOne add

---
+++ src/main/java/com/rzepecki/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.rzepecki.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        System.out.println("123");
+        return "index";
+    }
+}
